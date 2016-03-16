8108c634e2aa6698192a1690e910fa0efe8cbc46
Deblyn Prado <deblynprado@gmail.com>
2016-03-16 13:02:37 -0300

    Fixing paths and removing unused files

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css"
-sass_dir = "sass"
+sass_dir = "_sass"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 
